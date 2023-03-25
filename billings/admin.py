5ebc260467a7327d0b8e247ef11fce788fa964cf
shubham <shubhamsatyam54@gmail.com>
2023-03-25 07:52:56 +0530

    added inventory

--- billings/admin.py
+++ billings/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client,Product,Batch
 
 admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Batch)
+
 # Register your models here.
--- billings/urls.py
+++ billings/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("", DashboardView.as_view()),
     path("client/", ClientView.as_view(), name="clients"),
     path("client/add/", NewClientView.as_view()),
-    path("client/<pk>/edit", DetailClientView.as_view()),
-    path("client/<pk>/delete", DeleteClientView.as_view())
+    path("client/<pk>/edit/", DetailClientView.as_view()),
+
 
     # path('<pk>/update',ClientUpdateView.as_view())
 
